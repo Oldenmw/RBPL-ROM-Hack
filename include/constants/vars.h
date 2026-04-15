ef315fd5be6e146fa3a2b7f5e7b1c46e1e756cd9
Oldenmw <oldenmw@gmail.com>
2026-04-15 10:22:55 -0400

    Added berry trainer fight counter, weakened May's starting teams

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -97,7 +97,7 @@
 #define VAR_POKELOT_RND1                                 0x404B
 #define VAR_POKELOT_RND2                                 0x404C
 #define VAR_POKELOT_PRIZE_PLACE                          0x404D
-#define VAR_UNUSED_0x404E                                0x404E // Unused Var
+#define VAR_BERRY_FIGHT_COUNT                            0x404E // Used to track berry growth through trainer battles
 #define VAR_LOTAD_SIZE_RECORD                            0x404F
 #define VAR_LITTLEROOT_TOWN_STATE                        0x4050
 #define VAR_OLDALE_TOWN_STATE                            0x4051
